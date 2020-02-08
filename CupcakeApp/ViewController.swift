8043bf6b387183e0a2aedd7f817a936592da957b
LaureCompoint <compointlaure@gmail.com>
2020-02-08 15:15:43 +0100

    app phrase aléatoire cupcake recette

--- CupcakeApp/ViewController.swift
+++ CupcakeApp/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let subjects = ["Cupcake", "Muffin"]
+    let verbsAndObjects = ["aux pepites de chocolat", "aux myrtile et pepites de chocolat", "a la vanille et aux chocolat", "aux noisettes et chocolat", "aux chocolat", "nature et oreo", "aux framboise et à la vanille", "a la pistache et aux chocolat"]
 
+    @IBOutlet weak var sentenceLabel: UILabel!
+    @IBAction func cupcakeButton(_ sender: Any) {
+        makeResponse()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    private func makeResponse(){
+        let randomIndexSuject = Int(arc4random_uniform(UInt32(subjects.count)))
+        
+        let randomSubject = subjects[randomIndexSuject]
+        let randomIndexVerb = Int(arc4random_uniform(UInt32(verbsAndObjects.count)))
+        let randomVerb = verbsAndObjects[randomIndexVerb]
+        sentenceLabel.text = randomSubject + " " + randomVerb
     }
 
 
